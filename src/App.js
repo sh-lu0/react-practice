2f6481c1e17cae4eefb6bc4e3d2ab6be207473a6
sh-lu0 <sh-lu0@users.noreply.github.com>
2018-12-08 02:04:25 +0900

    フォームを追加

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
+import React, {
+  Component
+} from 'react'
+import {
+  SimpleForm
+} from './SimpleForm'
+import './App.css'
+// メイン画面のコンポーネント
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App' >
+        <SimpleForm / >
       </div>
-    );
+    )
   }
 }
-
-export default App;
